a92b47a3422b45d1b9f06878d571b67b1a81800c
pablo felipe <pabloletiane@gmail.com>
2025-10-21 15:23:32 -0300

    cy.visit() com a url do site não está pegando

---
+++ cypress/e2e/Cenario2-Cadastro/spec.cy.js
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+
+describe('Cenário 2 - Cadastro', () => {
+ 
+  it.only('Deve exibir erro ao tentar cadastrar com e-mail inválido', () => {
+    cy.visit('https://demo.nopcommerce.com/register?returnUrl=%2F '); 
+    //cy.get('#gender\\male').click();
+  
+  });
+
+  it('Deve exibir erro ao tentar cadastrar com first name inválido', () => {
+    // código do teste 2
+  });
+
+  it('Deve cadastrar com todos os campos válidos', () => {
+    // código do teste 3
+  });
+
+});
